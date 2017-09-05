77b7c6cf7c1280d076f21e51e829de7204a41c55
Mitchell Long and Snow Vilay <snow@>
2017-09-05 14:09:27 -0700

    add use of string class versus using public

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,8 +1,9 @@
-
-public def title_case(title)
-    split_title = title.split
-    split_title.each do |word|
-    word.capitalize!
+class String
+    def title_case()
+      split_title = self.split
+      split_title.each do |word|
+      word.capitalize!
+    end
+    split_title.join(" ")
   end
-  split_title.join(" ")
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -4,9 +4,9 @@ require('pry')
 
 describe('#title_case') do
   it("returns input with the first letter capitalized") do
-    expect(title_case("toy story")).to(eq("Toy story"))
+    expect(("toy").title_case).to(eq("Toy"))
   end
   it("capitalizes the first letter of all words in a multiple word title") do
-    expect(title_case("toy story")).to(eq("Toy Story"))
+    expect(("toy story").title_case).to(eq("Toy Story"))
   end
 end
